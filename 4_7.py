b322461f3660bcffedca4c58b526bb90855411ce
AntonD <antonderenok@yandex.ru>
2022-02-10 15:05:23 +0300

    lesson 4

---
+++ 4_7.py
@@ -0,0 +1,10 @@
+def fact(n):
+    f = (el for el in range(1, n + 1))
+    fact = 1
+    for i in f:
+        fact *= i
+        yield i, fact
+
+
+for num, j in fact(int(input('Введите N: '))):
+    print(f"{num}!={j}")
